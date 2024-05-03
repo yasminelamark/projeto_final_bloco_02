8f242b61431ab67b64897cdcaf1d83ff61ecd55e
Yasmine_lamark <yasmine_lamark@hotmail.com>
2024-05-03 14:13:20 -0300

    task 01 farmacia

---
+++ src/app.module.ts
@@ -0,0 +1,21 @@
+/* eslint-disable prettier/prettier */
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [TypeOrmModule.forRoot({
+    type: 'mysql',
+    host: 'localhost',
+    port: 3306,
+    username: 'root',
+    password: 'root',
+    database: 'db_projetofarmacia',
+    entities: [],
+    synchronize: true,
+  })
+
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
